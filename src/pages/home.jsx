0b9c27ebcb4d90b97ec07555108f42157ffdf089
MaxFilsR <maxfilsr@gmail.com>
2025-10-24 00:10:04 -0400

    stylized landing page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,37 @@
+import React from "react";
+import { Link } from "react-router";
+
 const HomePage = () => {
+  const randomImage = `https://picsum.photos/1920/1080?random=${Math.floor(
+    Math.random() * 1000
+  )}`;
+
   return (
-    <>
-      <div></div>
-      <h1>Bank Of React</h1>
-    </>
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${randomImage})` }}
+    >
+      <div className="overlay">
+        <h1 className="title">Welcome to the Bank of React</h1>
+        <div className="quick-nav">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/userProfile">
+            Profile
+          </Link>
+          <Link className="link" to="/login">
+            Login
+          </Link>
+          <Link className="link" to="/credits">
+            Credits
+          </Link>
+          <Link className="link" to="/debits">
+            Debits
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
