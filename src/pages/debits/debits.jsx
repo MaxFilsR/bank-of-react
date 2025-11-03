566e10f3cffa4063a0fd92e9e4514412c1476890
MaxFilsR <maxfilsr@gmail.com>
2025-11-02 20:10:55 -0500

    fixed spelling of description

--- src/pages/debits/debits.jsx
+++ src/pages/debits/debits.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import { useState } from "react";
 
 const DebitsPage = ({ balance, addDebit, debits }) => {
-  const [description, setDescription]=useState('');
-  const[amount, setAmount]=useState('');
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
 
-  const handleChanges=(event)=>{
+  const handleChanges = (event) => {
     event.preventDefault();
-    const NumericAmount=parseFloat(amount);
-    if(NumericAmount>0 && description){
-      const newDebit ={
+    const NumericAmount = parseFloat(amount);
+    if (NumericAmount > 0 && description) {
+      const newDebit = {
         description: description,
-        amount: NumericAmount
+        amount: NumericAmount,
       };
       addDebit(newDebit);
-      setDescription('');
-      setAmount('');
+      setDescription("");
+      setAmount("");
+    } else {
+      alert("Please fill description and amount.");
     }
-      else{
-        alert("Please fill description and amount.")
-      }
   };
-
 
   return (
     <div className="dashboard-page-content-container">
@@ -34,33 +32,46 @@ const DebitsPage = ({ balance, addDebit, debits }) => {
           <div>
             <label>Description: </label>
             <input
-            type="text"
-            placeholder="Enter description"
-            value={description}
-            onChange={(event)=>setDescription(event.target.value)}
-
+              type="text"
+              placeholder="Enter description"
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
             />
           </div>
           <div>
             <label>Amount: </label>
             <input
-            type="number"
-            placeholder="0.00"
-            value={amount}
-            onChange={(event)=>setAmount(event.target.value)}
+              type="number"
+              placeholder="0.00"
+              value={amount}
+              onChange={(event) => setAmount(event.target.value)}
             />
-            </div>
-            <button type="submit"> Add Debit</button>
-          </form>
-          <h3> Debit History</h3>
-          {debits.map((debit,index)=>(
-            <div key={index} style={{border: '1px solid #ccc', margin: '10px', padding:'10px'}}>
-              <p><strong>Description:</strong>{debit.descreption}</p>
-              <p><strong> Amount:</strong>${debit.amount.toLocaleString("en-US")}</p>
-              <p><strong> Date:</strong>{debit.date}</p>
-            </div>
-          ))}
-
+          </div>
+          <button type="submit"> Add Debit</button>
+        </form>
+        <h3> Debit History</h3>
+        {debits.map((debit, index) => (
+          <div
+            key={index}
+            style={{
+              border: "1px solid #ccc",
+              margin: "10px",
+              padding: "10px",
+            }}
+          >
+            <p>
+              <strong>Description:</strong>
+              {debit.description}
+            </p>
+            <p>
+              <strong> Amount:</strong>${debit.amount.toLocaleString("en-US")}
+            </p>
+            <p>
+              <strong> Date:</strong>
+              {debit.date}
+            </p>
+          </div>
+        ))}
       </main>
     </div>
   );
